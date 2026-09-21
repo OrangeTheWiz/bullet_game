e3e0fe088cda3001f2010803caed56f918489c1c
OrangeTheWiz <bnw357@gmail.com>
2026-09-21 22:16:58 +0000

    change font on line 233

--- scripting.js
+++ scripting.js
@@ -230,7 +230,7 @@ function Loop()
      }
     }
     context.reset()
-    context.font = "20px Ariel"
+    context.font = "20px fantasy"
 
     context.fillStyle = "orange"
     context.drawImage(face, FaceX, FaceY)
