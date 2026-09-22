65a938e5314c3a6d4bbfbdf0ca66914b17a16d5f
OrangeTheWiz <bnw357@gmail.com>
2026-09-22 03:15:39 +0000

    Update scripting.js

--- scripting.js
+++ scripting.js
@@ -230,7 +230,7 @@ function Loop()
      }
     }
     context.reset()
-    context.font = "20px sans-serif"
+    context.font = "20px Serif"
 
     context.fillStyle = "orange"
     context.drawImage(face, FaceX, FaceY)
